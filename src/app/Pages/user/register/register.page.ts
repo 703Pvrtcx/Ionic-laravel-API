8d882a490eaf58f29f23f7558761a0319e1a1457
703Pvrtcx <kasaokapapkies@gmail.com>
2021-09-09 11:12:12 +0200

    Added Register Page/ withno error catching

--- src/app/Pages/user/register/register.page.ts
+++ src/app/Pages/user/register/register.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from 'src/app/Services/User/authentication.service';
 import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-register',
@@ -12,7 +13,20 @@ export class RegisterPage implements OnInit {
   form:  FormGroup;
   constructor(private AuthService: AuthenticationService) {}
 ngOnInit() {
- 
+  this.form =  new FormGroup({
+    name : new FormControl(null,[Validators.required]),
+    email : new FormControl(null,[Validators.required]),
+    password : new FormControl(null,[Validators.required]), 
+    password_confirmation : new FormControl(null,[Validators.required]),
+  });
   }
-
+  async submitRegister(){
+    this.AuthService.register(this.form.value).
+      pipe(take(1))
+      .subscribe((res)=>{
+        console.log("Token: ",res['token']);
+        console.log("User: ",res['user']);
+        this.form.reset();
+    });
+  }
 }
