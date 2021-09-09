41e282df163d8852ce0d62739a1d758d7f322adc
703Pvrtcx <kasaokapapkies@gmail.com>
2021-09-09 13:59:20 +0200

    Added Login user as sign-in user

--- src/app/Pages/user/register/register.page.ts
+++ src/app/Pages/user/register/register.page.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from 'src/app/Services/User/authentication.service';
 import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 import { take } from 'rxjs/operators';
+import { Router } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-register',
@@ -11,22 +14,29 @@ import { take } from 'rxjs/operators';
 export class RegisterPage implements OnInit {
 
   form:  FormGroup;
-  constructor(private AuthService: AuthenticationService) {}
+  constructor(
+    private AuthService: AuthenticationService,
+    private route: Router) {}
+
 ngOnInit() {
   this.form =  new FormGroup({
-    name : new FormControl(null,[Validators.required]),
-    email : new FormControl(null,[Validators.required]),
-    password : new FormControl(null,[Validators.required]), 
-    password_confirmation : new FormControl(null,[Validators.required]),
+    name : new FormControl("Name",[Validators.required]),
+    email : new FormControl("01email@gmail.com",[Validators.required]),
+    password : new FormControl("password",[Validators.required]), 
+    password_confirmation : new FormControl("password1",[Validators.required]),
   });
   }
   async submitRegister(){
     this.AuthService.register(this.form.value).
       pipe(take(1))
       .subscribe((res)=>{
+        console.log('Success ===', res);
         console.log("Token: ",res['token']);
         console.log("User: ",res['user']);
         this.form.reset();
+        this.route.navigateByUrl('sign-in');
+    },error =>{
+      console.log('Error === Error',error.error['errors']);
     });
   }
 }
--- src/app/Pages/user/sign-in/sign-in.page.ts
+++ src/app/Pages/user/sign-in/sign-in.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from 'src/app/Services/User/authentication.service';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import { take } from 'rxjs/operators';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-sign-in',
@@ -7,9 +12,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignInPage implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  form:  FormGroup;
+  constructor(
+    private AuthService: AuthenticationService,
+    private route: Router) {}
+ngOnInit() {
+  this.form =  new FormGroup({
+    email : new FormControl("01email@gmail.com",[Validators.required]),
+    password : new FormControl("password",[Validators.required]), 
+  });
+  }
+  async submitLogin(){
+    this.AuthService.login(this.form.value).
+      pipe(take(1))
+      .subscribe((res)=>{
+            console.log('Success ===', res);
+            // console.log("Token: ",res['token']);
+            // console.log("User: ",res['user']);
+            this.form.reset();
+        },error =>{
+             console.log('Error === Error',error.error['errors']);
+       });
   }
 
 }
